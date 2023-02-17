3faaf7f37bc16952ae33f571343d9f613fcc1db7
hamed gandomkar <dev.hamedgk@yahoo.com>
2023-02-17 23:31:53 +0330

    put game in a loop

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ mod npc;
 mod human;
 mod win_check;
 mod npc_deffense_logic;
+mod possible_moves;
 
 use game_board::GameBoard;
 use tile_state::TileState;
 
 fn main() {
     let mut game_board = GameBoard::new(Some(TileState::Cross));
-    game_board.play();
+    loop{
+        game_board.play();
+        game_board.reset_game(None);
+    }
 
 }
